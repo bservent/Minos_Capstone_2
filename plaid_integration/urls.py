8347269784cddbccf6f050037661672c353b9f83
Brittany Servent <bservent@gmail.com>
2021-02-16 18:44:54 -0800

    urls updated

---
+++ plaid_integration/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('', include('frontend.urls')),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/', include('accounting.urls', namespace='accounting')),
+    path('api/v1/auth/', include('djoser.urls.base')),
+    path('api/v1/auth/', include('djoser.urls.authtoken')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
